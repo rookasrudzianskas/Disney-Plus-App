b8ae616169e27b39e85de92e093b59d5ad42f8e6
Rokas <rookas.rudzenskas@gmail.com>
2021-05-17 15:48:09 +0300

    Rokas

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -4,11 +4,42 @@ import styled from "styled-components";
 const Viewers = () => {
     return (
         <Container>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
-            <div>5</div>
+            <Wrap>
+                <img src="/images/viewers-disney.png" alt="" />
+                <video autoPlay={true} loop={true} playsInline={true}>
+                    <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
+                </video>
+            </Wrap>
+
+            <Wrap>
+                <img src="/images/viewers-pixar.png" alt="" />
+                <video autoPlay={true} loop={true} playsInline={true}>
+                    <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
+                </video>
+            </Wrap>
+
+            <Wrap>
+                <img src="/images/viewers-marvel.png" alt="" />
+                <video autoPlay={true} loop={true} playsInline={true}>
+                    <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
+                </video>
+            </Wrap>
+
+            <Wrap>
+                <img src="/images/viewers-starwars.png" alt="" />
+                <video autoPlay={true} loop={true} playsInline={true}>
+                    <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
+                </video>
+            </Wrap>
+
+            <Wrap>
+                <img src="/images/viewers-national.png" alt="" />
+                <video autoPlay={true} loop={true} playsInline={true}>
+                    <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
+                </video>
+            </Wrap>
+
+
         </Container>
     );
 };
@@ -25,5 +56,30 @@ const Container = styled.div`
   
   @media(max-width: 768px) {
     grid-template-columns: repeat(1, minmax(0, 1fr));
+  }
+`
+
+const Wrap = styled.div`
+  padding-top: 56.25%;
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+  rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  cursor: pointer;
+  overflow: hidden;
+  position: relative;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+  
+  img {
+    inset: 0px;
+    display: block;
+    height: 100%;
+    object-fit: contain;
+    opacity: 1;
+    position: absolute;
+    transition: opacity 500ms ease-in-out 0s;
+    width: 100%;
+    z-index: 1;
+    top: 0;
   }
 `
