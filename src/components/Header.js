ce79e9d3077572ddcbe5a8e75bd2273807112db8
Rokas <rookas.rudzenskas@gmail.com>
2021-05-17 10:09:17 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,6 +15,27 @@ const Header = (props) => {
                     <span>HOME</span>
                 </a>
 
+                <a>
+                    <img src="/images/search-icon.svg" />
+                    <span>SEARCH</span>
+                </a>
+                <a>
+                    <img src="/images/watchlist-icon.svg" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a>
+                    <img src="/images/original-icon.svg" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a>
+                    <img src="/images/movie-icon.svg" />
+                    <span>MOVIES</span>
+                </a>
+                <a>
+                    <img src="/images/series-icon.svg" />
+                    <span>SERIES</span>
+                </a>
+
             </NavMenu>
 
         </Nav>
@@ -52,69 +73,44 @@ const Logo = styled.div`
   }
 `
 const NavMenu = styled.div`
-  align-items: center;
   display: flex;
-  flex-flow: row nowrap;
-  height: 100%;
-  justify-content: flex-end;
-  margin: 0px;
-  padding: 0px;
-  position: relative;
-  margin-right: auto;
+  flex: 1;
   margin-left: 25px;
-  
+  align-items: center;
   a {
     display: flex;
     align-items: center;
     padding: 0 12px;
-  }
-  
-  img {
-    height: 20px;
-    min-width: 20px;
-    width: 20px;
-    z-index: auto;
-    align-items: center;
-  }
-  
-  span {
-    color: rgb(249, 249, 249);
-    font-size: 13px;
     cursor: pointer;
-    letter-spacing: 1.42px;
-    line-height: 1.08;
-    padding: 2px 0px;
-    white-space: nowrap;
-    position: relative;
-  
-  &:before {
-    background-color: rgb(249, 249, 249);
-    border-radius: 0px 0px 4px 4px;
-    bottom: -6px;
-    content: "";
-    height: 2px;
-    left: 0px;
-    opacity: 0;
-    position: absolute;
-    right: 0px;
-    transform-origin: left center;
-    transform: scaleX(0);
-    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
-    visibility: hidden;
-    width: auto;
+    img {
+      height: 20px;
     }
-  }
-  &:hover {
-    span:before {
-      transform: scaleX(1);
-      visibility: visible;
-      opacity: 1 !important;
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      position: relative;
+      &:after {
+        content: "";
+        height: 2px;
+        background: white;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        opacity: 0;
+        transform-origin: left center;
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        transform: scaleX(0);
+      }
     }
-  }
-  
-  @media(max-width: 768px) {
-    display: none;
+    &:hover {
+      span:after {
+        transform: scaleX(1);
+        opacity: 1;
+      }
+    }
   }
 `
+
 
 
