e0b866e3084d265ab579a1f30c0f743d5c0bbe60
Rokas <rookas.rudzenskas@gmail.com>
2021-05-17 10:49:00 +0300

    Rokas

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -14,9 +14,15 @@ const userSlice = createSlice({
         // then the user logs in, remember all the stuff from that object and store in the data layer
         setUserLoginDetails: (state, action) => {
             // sets all the needed data into the state of the user
-            state.name = action.payload.name
-            state.email = action.payload.email
-            state.photo = action.payload.photo
+            state.name = action.payload.name;
+            state.email = action.payload.email;
+            state.photo = action.payload.photo;
+        },
+
+     setSignOutState: state => {
+            state.name = null,
+            state.email = null,
+            state.photo = null;
         }
     }
-})
+});
