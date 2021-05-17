feee10ef693356757959ae894296cc0f2bbafbc1
Rokas <rookas.rudzenskas@gmail.com>
2021-05-17 14:51:14 +0300

    Rokas

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,32 @@ import styled from "styled-components";
 const Home = (props) => {
     return (
         <div>
-            <h1>Home 🚀</h1>
+            <Container>
+                <h1>Hello</h1>
+                <h1>Hello</h1>
+                <h1>Hello</h1>
+                <h1>Hello</h1>
+            </Container>
         </div>
     );
 };
 
 export default Home;
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+  
+  &:after {
+    background: url("/images/home-background.png") center center / cover no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`
