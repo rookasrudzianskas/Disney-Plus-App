ef661c36afbf66427a9d591a13f173da49212126
Rokas <rookas.rudzenskas@gmail.com>
2021-05-17 10:47:18 +0300

    Rokas

--- src/app/store.js
+++ src/app/store.js
@@ -5,5 +5,6 @@ export default configureStore({
     reducer: {},
     middleware: getDefaultMiddleware({
         serializableCheck: false,
-    })
-})
+    }),
+});
+
---
+++ src/features/user/userSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// for the user, everything is empty at start
+const initialState = {
+    name: '',
+    email: '',
+    photo: '',
+};
+
+const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        // then the user logs in, remember all the stuff from that object and store in the data layer
+        setUserLoginDetails: (state, action) => {
+            // sets all the needed data into the state of the user
+            state.name = action.payload.name
+            state.email = action.payload.email
+            state.photo = action.payload.photo
+        }
+    }
+})
