82a0c8479b69cc4f2f3721d71af9550c598a4ae7
Rokas <rookas.rudzenskas@gmail.com>
2021-05-17 09:40:55 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Login from "./components/Login";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
         <Router>
+            <Header />
             <Switch>
                 <Route exact path="/">
                     <Login />
---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Header = (props) => {
+    return (
+        <Nav>
+            <Logo>
+                <img src="/images/logo.svg" alt=""/>
+            </Logo>
+
+        </Nav>
+    );
+};
+
+export default Header;
+
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
+`
+
+const Logo = styled.div`
+  padding: 0;
+  width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  font-size: 0;
+  display: inline-block;
+  img {
+    display: block;
+    width: 100%;
+  }
+  
+  
+`
