52a88c7322f092b8e189553a2c3c375e332c7927
Rokas <rookas.rudzenskas@gmail.com>
2021-05-17 08:27:35 +0300

    Rokas

--- src/components/Login.js
+++ src/components/Login.js
@@ -16,9 +16,22 @@ export default MyComponent;
 const Container = styled.section`
   overflow: hidden;
   display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 100vh;
   
 `
 
 const Content = styled.div`
-  
+  margin-bottom: 10vw;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 80px 40px;
+  height: 100%;
 `
