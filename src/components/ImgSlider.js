0c420f16e86912748156783f27f45e6018709d9b
Rokas <rookas.rudzenskas@gmail.com>
2021-05-17 15:04:19 +0300

    Rokas:

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -10,11 +10,25 @@ const ImgSlider = () => {
         dots: true,
         infinite: true,
         speed: 500,
-
+        slidesToScroll: 1,
+        autoplay: true,
     }
     return (
         <div>
-            Image Slider
+            <Slider {...settings}>
+
+                <div>
+                    <h3>1</h3>
+                </div>
+
+                <div>
+                    <h3>2</h3>
+                </div>
+
+                <div>
+                    <h3>3</h3>
+                </div>
+            </Slider>
         </div>
     );
 };
