30f3d99a7c4288a3d6f4667a7d57a1c9f18503a8
Rokas <rookas.rudzenskas@gmail.com>
2021-05-17 14:56:30 +0300

    Rokas

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import styled from "styled-components";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const ImgSlider = () => {
+    // defining settings of the image slider
+    let settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+
+    }
     return (
         <div>
             Image Slider
