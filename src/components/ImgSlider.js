f65214c197efedd33a784e089fe3dc94dafec762
Rokas <rookas.rudzenskas@gmail.com>
2021-05-17 15:22:47 +0300

    Rokas

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -16,17 +16,30 @@ const ImgSlider = () => {
     return (
             <Carousel {...settings}>
 
-                <div>
-                    <h3>1</h3>
-                </div>
+                <Wrap>
+                    <a>
+                        <img src="/images/slider-badging.jpg" alt="" />
+                    </a>
+                </Wrap>
 
-                <div>
-                    <h3>2</h3>
-                </div>
+                <Wrap>
+                    <a>
+                        <img src="/images/slider-scale.jpg" alt="" />
+                    </a>
+                </Wrap>
 
-                <div>
-                    <h3>3</h3>
-                </div>
+                <Wrap>
+                    <a>
+                        <img src="/images/slider-badag.jpg" alt="" />
+                    </a>
+                </Wrap>
+
+                <Wrap>
+                    <a>
+                        <img src="/images/slider-scales.jpg" alt="" />
+                    </a>
+                </Wrap>
+
             </Carousel>
     );
 };
@@ -35,22 +48,57 @@ export default ImgSlider;
 
 const Carousel = styled(Slider)`
   margin-top: 20px;
-  
   & > button {
     opacity: 0;
     height: 100%;
     width: 5vw;
     z-index: 1;
-    
     &:hover {
       opacity: 1;
       transition: opacity 0.2s ease 0s;
     }
   }
-  
   ul li button {
     &:before {
       font-size: 10px;
+      color: rgb(150, 158, 171);
     }
   }
+  li.slick-active button:before {
+    color: white;
+  }
+  // to see the opcoming one too
+  .slick-list {
+    overflow: initial;
+  }
+  
+  .slick-prev {
+    left: -75px;
+  }
+  
+  .slick-next {
+    right: -75px;
+  }
+`
+
+
+const Wrap = styled.div`
+  border-radius: 4px;
+  cursor: pointer;
+  position: relative;
+
+  a {
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    cursor: pointer;
+    display: block;
+    position: relative;
+    padding: 4px;
+
+
+  img {
+    width: 100%;
+    height: 100%;
+    } 
+  }  
 `
