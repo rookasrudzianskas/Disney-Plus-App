6997eec1c1110d9df03e8bcb1c3eb8df5261a0bd
Rokas <rookas.rudzenskas@gmail.com>
2021-05-18 11:09:44 +0300

    Rokas

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
+import movieReducer from "../features/movie/movieSlice";
 // setting up the redux store
 // initializes the data layer
 export default configureStore({
     reducer: {
-        user: userReducer
+        user: userReducer,
+        movie: movieReducer
     },
     middleware: getDefaultMiddleware({
         serializableCheck: false,
--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/storage';
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCQCA7wHBo_XVEBtdynbqW4XK2EEASL-7I",
-    authDomain: "rookas-disney-plus-app.firebaseapp.com",
-    projectId: "rookas-disney-plus-app",
-    storageBucket: "rookas-disney-plus-app.appspot.com",
-    messagingSenderId: "979099827737",
-    appId: "1:979099827737:web:3c509b64a0070e6e5cb488",
-    measurementId: "G-7R40HFE9XZ"
+    apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
+    authDomain: "disneyplus-clone-a33d5.firebaseapp.com",
+    projectId: "disneyplus-clone-a33d5",
+    storageBucket: "disneyplus-clone-a33d5.appspot.com",
+    messagingSenderId: "37918794208",
+    appId: "1:37918794208:web:dbe9842dfe1dda522a4b85",
+    measurementId: "G-DRVLJKWRWG",
 };
 //
 const firebaseApp = firebase.initializeApp(firebaseConfig);
